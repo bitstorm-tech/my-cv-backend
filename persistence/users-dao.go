08c8f205c6e9b97797cafd273e41dc55e46ce056
Josef Bauer <bauerjoe@gmx.de>
2018-03-07 23:28:05 +0100

    Added persistence layer for users

---
+++ persistence/users-dao.go
@@ -0,0 +1,81 @@
+package persistence
+
+import (
+	"errors"
+	"fleet-commander-backend-go/models"
+	"fmt"
+)
+
+// InsertNewUser inserts a new user. When either the Username or the Email of the
+// user already exists, the functions returns an error
+func InsertNewUser(user *models.User) error {
+	passwordHash := user.GetPasswordHash()
+	if len(passwordHash) == 0 {
+		fmt.Println("ERROR: Invalid password hash", passwordHash)
+		return errors.New("")
+	}
+
+	user.Password = passwordHash
+	fmt.Println("Insert new user:", user)
+
+	database := getArangoDatabase()
+	query := "FOR u IN users FILTER LOWER(u.Email) == LOWER(@email) OR LOWER(u.Username) == LOWER(@username) RETURN u"
+	bindings := bindingVariables{
+		"email":    user.Email,
+		"username": user.Username,
+	}
+
+	cursor, err := database.Query(nil, query, bindings)
+	if err != nil {
+		fmt.Println("ERROR: invalid query:", err)
+		return err
+	}
+
+	if cursor.HasMore() {
+		fmt.Println("WARN: user already exists")
+		return fmt.Errorf("User with username=%s or email=%s already exists", user.Username, user.Email)
+	}
+
+	collection, err := database.Collection(nil, "users")
+	if err != nil {
+		fmt.Println("ERROR: can't open collection:", err)
+		return err
+	}
+
+	_, err = collection.CreateDocument(nil, user)
+	if err != nil {
+		fmt.Println("ERROR: can't create user:", err)
+		return err
+	}
+
+	return nil
+}
+
+// GetUserByEmail returns the user that matches with the given email
+func GetUserByEmail(email string) (*models.User, error) {
+	fmt.Println("Get user by email:", email)
+
+	database := getArangoDatabase()
+	query := "FOR u IN users FILTER LOWER(u.Email) == LOWER(@email) RETURN u"
+	bindings := bindingVariables{
+		"email": email,
+	}
+	cursor, err := database.Query(nil, query, bindings)
+	if err != nil {
+		fmt.Println("ERROR: no user found with email:", email, err)
+		return nil, err
+	}
+
+	user := new(models.User)
+	_, err = cursor.ReadDocument(nil, user)
+	if err != nil {
+		fmt.Println("ERROR: can't read user from cursor:", err)
+		return nil, err
+	}
+
+	if cursor.HasMore() {
+		fmt.Printf("ERROR: found multiple users with email %s, will use first one\n", email)
+	}
+
+	return user, nil
+}
---
+++ persistence/utils.go
@@ -0,0 +1,75 @@
+package persistence
+
+import (
+	"fmt"
+	"os"
+
+	driver "github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
+)
+
+type bindingVariables map[string]interface{}
+
+var arangoClient driver.Client
+var arangoDatabase driver.Database
+var arangoCollections map[string]driver.Collection
+
+func getArangoClient() driver.Client {
+	if arangoClient == nil {
+		fmt.Println("Initializing ArangoDB client")
+		endpoint := "http://localhost:8529"
+
+		connectionConfig := http.ConnectionConfig{
+			Endpoints: []string{endpoint},
+		}
+
+		connection, err := http.NewConnection(connectionConfig)
+		if err != nil {
+			fmt.Println("ERROR: can't create connection:", err)
+			os.Exit(1)
+		}
+
+		clientConfig := driver.ClientConfig{
+			Connection:     connection,
+			Authentication: driver.BasicAuthentication("root", "root"),
+		}
+
+		arangoClient, err = driver.NewClient(clientConfig)
+		if err != nil {
+			fmt.Println("ERROR: can't create ArangoDB client:", err)
+			os.Exit(1)
+		}
+	}
+
+	return arangoClient
+}
+
+func getArangoDatabase() driver.Database {
+	if arangoDatabase == nil {
+		fmt.Println("Initializing ArangoDB database")
+		client := getArangoClient()
+		var err error
+		arangoDatabase, err = client.Database(nil, "my-cv")
+		if err != nil {
+			fmt.Println("ERROR: can't get database:", err)
+			os.Exit(1)
+		}
+	}
+
+	return arangoDatabase
+}
+
+func getArangoCollection(name string) driver.Collection {
+	if arangoCollections[name] == nil {
+		fmt.Println("Initialize ArangoDB collection:", name)
+		database := getArangoDatabase()
+		collection, err := database.Collection(nil, name)
+		if err != nil {
+			fmt.Println("ERROR: can't get collection", name, err)
+			os.Exit(1)
+		}
+		arangoCollections[name] = collection
+	}
+
+	return arangoCollections[name]
+}
